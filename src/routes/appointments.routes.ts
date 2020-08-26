42c9d55a41109142db69c0747c61247c4e8dd7bd
Gabriel Kim <gabrielkim13@gmail.com>
2020-08-25 22:23:49 -0300

    Add CreateUserService (with encrypted password)

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -4,6 +4,7 @@ import { getCustomRepository } from 'typeorm';
 
 import CreateAppointmentService from '../services/CreateAppointmentService';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import usersRouter from './users.routes';
 
 const appointmentsRouter = Router();
 
@@ -17,14 +18,14 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
     const createAppointmentService = new CreateAppointmentService();
 
     const appointment = await createAppointmentService.execute({
-      provider,
+      provider_id,
       date: parsedDate,
     });
 
