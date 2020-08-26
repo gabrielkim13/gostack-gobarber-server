ff393dc8c630c051bc7432e22aea14040bf1265d
Gabriel Kim <gabrielkim13@gmail.com>
2020-08-26 19:43:50 -0300

    Add user avatar storage

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
+import uploadConfig from './config/upload';
+
 const app = express();
+
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 export default app;
