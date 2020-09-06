8e8b91931a38d98089ade9a1240dffbad15f73b4
Gabriel Kim <gabrielkim13@gmail.com>
2020-09-06 10:15:57 -0300

    Finish backend for frontend integration

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
+import AppError from '../errors/AppError';
+
 import authConfig from '../config/auth';
 
 interface TokenPayload {
@@ -17,7 +19,7 @@ export default function ensureAuthenticated(
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error('Missing JWT token.');
+    throw new AppError('Missing JWT token.', 401);
   }
 
   const [, token] = authHeader.split(' ');
@@ -31,6 +33,6 @@ export default function ensureAuthenticated(
 
     return next();
   } catch (err) {
-    throw new Error('Invalid JWT token.');
+    throw new AppError('Invalid JWT token.', 401);
   }
 }
--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
 
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 import uploadConfig from '../config/upload';
@@ -18,7 +20,7 @@ class UpdateUserAvatarService {
     const user = await usersRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('User does not exist.');
+      throw new AppError('User does not exist.');
     }
 
     if (user.avatar) {
