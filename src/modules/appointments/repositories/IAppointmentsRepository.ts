7a69d3ddebebd48883cf86d1919ea29c6ea36e15
Gabriel Kim <gabrielkim13@gmail.com>
2020-09-24 19:05:35 -0300

    FindByDate method on Appointments repo checks provider_id

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -6,7 +6,7 @@ import IFindAllInMonthFromProvider from '../dtos/IFindAllInMonthFromProviderDTO'
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
-  findByDate(date: Date): Promise<Appointment | undefined>;
+  findByDate(date: Date, provider_id: string): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProvider,
   ): Promise<Appointment[]>;
