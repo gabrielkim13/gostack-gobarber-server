37a059f1e66ad7d7f244b9641d738c58b7a57e60
Gabriel Kim <gabrielkim13@gmail.com>
2020-09-24 19:11:36 -0300

    Display past days as unavailable on MonthAvailability Service

--- src/modules/appointments/services/ListProvidersMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProvidersMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
@@ -41,11 +41,17 @@ class ListProvidersMonthAvailabilityService {
     );
 
     const availability = daysInMonth.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       const appointmentsInDay = appointmentsInMonth.filter(
         appointment => getDate(appointment.date) === day,
       );
 
-      return { day, available: appointmentsInDay.length < 10 };
+      return {
+        day,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
+      };
     });
 
     return availability;
