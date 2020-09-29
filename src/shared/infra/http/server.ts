e4d4051250e65d5a73f5ba6092e5af75015b4da2
Gabriel Kim <gabrielkim13@gmail.com>
2020-09-29 18:45:35 -0300

    Apply class transformation to providers

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -19,9 +19,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(rateLimiter);
-
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+
+app.use(rateLimiter);
 app.use(routes);
 
 app.use(errors());
